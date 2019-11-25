94741ae35e55c357af69fd0206760c4586cf6835
NikitaDonets <nikitadonets@gmail.com>
2019-11-25 13:42:51 +0300

    hello вася (java)

--- try.java
+++ try.java
@@ -1,5 +1,5 @@
 public class Try{
-public static void main(String[] args) {
-        System.out.println("Hello вася");
+	public static void main(String[] args) {
+        	System.out.println("Hello вася");
         }
-    }
+}
