04ae150283a8ff1c0e33c85d370f2158e0ec3c5a
NikitaDonets <nikitadonets@gmail.com>
2019-11-26 17:41:24 +0300

    add figure king

--- Chess.java
+++ Chess.java
@@ -1,26 +1,3 @@
-/*
-public class Cell {
-	
-	public abstract void passibleMove(int[] a);
-	
-}
-
-public class Pawn extends Figure {
-	@Override
-	public void passibleMove(int[] a){
-		int[][] array = {{0, 1}, {-1, 1}, {1, 1}};
-		int[] passible = {0, 0, 0};
-		
-	}
-}
-
-
-public class Chess{
-	public static void main(String[] args) {
-        	System.out.println("Hello вася");
-        }
-}
-*/
 
 class SimpleChess {
 	static Cells[] Cell = new Cells[256];
@@ -96,10 +73,51 @@ class SimpleChess {
 		Cell[185] = new Cells(1, 2, 3, 3);
 		Cell[186] = new Cells(1, 2, 4, 5);
 		Cell[187] = new Cells(1, 2, 5, 7);
+		/*
 		System.out.println("# \t In space \t Color \t Type \t Index");
 		for (int i=0; i<256; i++){
 			System.out.println(i+"\t"+Cell[i].space+"\t"+Cell[i].color+"\t"+Cell[i].type+"\t"+Cell[i].index);
 		}
+		*/
+		/*Заполняем массив с индексами*/
+		int[][] positionfigures = new int[2][16];
+		positionfigures[0][0] = 72;
+		positionfigures[0][1] = 71;
+		positionfigures[0][2] = 70;
+		positionfigures[0][3] = 73;
+		positionfigures[0][4] = 69;
+		positionfigures[0][5] = 74;
+		positionfigures[0][6] = 68;
+		positionfigures[0][7] = 75;
+		positionfigures[0][8] = 84;
+		positionfigures[0][9] = 85;
+		positionfigures[0][10] = 86;
+		positionfigures[0][11] = 87;
+		positionfigures[0][12] = 88;
+		positionfigures[0][13] = 89;
+		positionfigures[0][14] = 90;
+		positionfigures[0][15] = 91;
+		
+		positionfigures[1][0] = 184;
+		positionfigures[1][1] = 183;
+		positionfigures[1][2] = 182;
+		positionfigures[1][3] = 185;
+		positionfigures[1][4] = 181;
+		positionfigures[1][5] = 186;
+		positionfigures[1][6] = 180;
+		positionfigures[1][7] = 187;
+		positionfigures[1][8] = 164;
+		positionfigures[1][9] = 165;
+		positionfigures[1][10] = 166;
+		positionfigures[1][11] = 167;
+		positionfigures[1][12] = 168;
+		positionfigures[1][13] = 169;
+		positionfigures[1][14] = 170;
+		positionfigures[1][15] = 171;
+		
+		/*проверка king*/
+		Figure test = new King();
+		test.passibleMove(Cell, 85);
 	}
 }
 
@@ -113,3 +131,36 @@ class Cells{
 		this.index = index;
 	}
 }
+
+abstract class Figure {
+	
+	abstract void passibleMove(Cells[] a, int b);
+	
+}
+
+class King extends Figure {
+	@Override
+	void passibleMove(Cells[] a, int b){
+		int[] array = {-17, -16, -15, -1, 1, 15, 16, 17};
+		int c, j=0;
+		for (int i=0; i<8;i++){
+			c = b + array[i];
+			if (a[c].space == 1){
+				if (a[c].color != a[b].color){
+					j++;
+					System.out.println(c);
+				}
+			}
+		}
+	}
+}
+
+/*public class Queen extends Figure {
+	@Override
+	public void passibleMove(int[] a){
+		int[][] array = {{0, 1}, {-1, 1}, {1, 1}};
+		int[] passible = {0, 0, 0};
+		
+	}
+}
+*/
